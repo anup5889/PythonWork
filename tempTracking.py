3b8d669d3726691d5ea0cb6ad7e5a0e3b88a2e58
anup5889 <anup5889@gmail.com>
2016-05-05 16:54:46 -0700

    Reduced the complexity of  get_max(), get_min() from O(n) to O(1)

--- tempTracking.py
+++ tempTracking.py
@@ -1,25 +1,31 @@
 
 class TempTracker:
     def __init__(self):
-        self.tempList=[]
+        self.min_temp=None
+        self.max_temp=None
+        self.sum_temp=0
+        self.count=0
     def insert(self,temp):
-        if temp>=0 and temp<=110:
-        	self.tempList.append(temp)
+        self.count+=1
+        self.sum_temp+=temp
+        if (self.max_temp is None) or (temp>self.max_temp):
+            self.max_temp=temp
+        if (self.min_temp is None) or (temp<self.min_temp):
+            self.min_temp=temp
+
     def get_max(self):
-        if len(self.tempList)>0:
-        	return max(self.tempList)
-        else: 
-            raise Exception("Empty List of Temperature")
+        return self.max_temp
     def get_min(self):
-        return min(self.tempList)
+        return self.min_temp
     def get_mean(self):
-        return sum(self.tempList)/len(self.tempList)
+        return self.sum_temp/float(self.count)
     def get_mode(self):
         return self.tempList[len(self.tempList)/2]
 tmpSystem=TempTracker()
 
 tmpSystem.insert(109)
+tmpSystem.insert
 print tmpSystem.get_max()
 print tmpSystem.get_min()
 print tmpSystem.get_mean()
-print tmpSystem.get_mode()
+#print tmpSystem.get_mode()
